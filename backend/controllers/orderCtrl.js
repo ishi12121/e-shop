ebacd9cb144231c963f4d12d5bf0f11b63e7de47
ishi12121 <isht1402@gmail.com>
2023-11-13 04:06:58 +0530

    Add Stripe payment functionality

--- backend/controllers/orderCtrl.js
+++ backend/controllers/orderCtrl.js
@@ -3,9 +3,16 @@ import asyncHandler from "express-async-handler";
 import Order from "../model/Order.js";
 import User from "../model/User.js";
 import Product from "../model/Product.js";
+import Stripe from "Stripe";
+import dotenv from "dotenv";
+dotenv.config();
 //@desc create orders
 //@route POST /api/v1/orders
 //@access private
+
+//stripe INSTANCE
+const stripe = new Stripe(process.env.STRIPE_KEY);
+
 export const createOrderCtrl = asyncHandler(async (req, res) => {
   //Get the payload(customer, orderItems,shippingAddress,totalPrice);
   const { customer, orderItems, shippingAddress, totalPrice } = req.body;
@@ -43,17 +50,38 @@ export const createOrderCtrl = asyncHandler(async (req, res) => {
   });
   //push order into user
   user.orders.push(order?._id);
- await user.save();
+  await user.save();
+  //convert order items to have same structure that stripe need
+  const convertedOrders = orderItems.map((item) => {
+    return {
+      price_data: {
+        currency: "usd",
+        product_data: {
+          name: item?.name,
+          description: item?.description,
+        },
+        unit_amount: item?.price * 100,
+      },
+      quantity: item?.qty,
+    };
+  });
   //make payment (stripe)
-
+  const session = await stripe.checkout.sessions.create({
+    line_items: convertedOrders,
+    mode: 'payment',
+    success_url: 'http://localhost:3000/success',
+    cancel_url:'http://localhost:3000/cancel',
+  });
+  res.send({ url: session.url });
+  
   //payment webhook
 
   //update the user webhook
-  res.json({
-    success: true,
-    message: "order created",
-    order,
-    user,
+ // res.json({
+   // success: true,
+  //  message: "order created",
+  //  order,
+  //  user,
     
-  });
+ // });
 });
