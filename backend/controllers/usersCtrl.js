1d7f824c56a726888ad17323ce1e96da93c637e1
ishi12121 <isht1402@gmail.com>
2023-11-13 06:29:28 +0530

    Refactor getUserProfileCtrl to fetch user and orders

--- backend/controllers/usersCtrl.js
+++ backend/controllers/usersCtrl.js
@@ -63,14 +63,14 @@ export const loginUserCtrl = asyncHandler(async (req, res) => {
 // @access  Private
 
 export const getUserProfileCtrl = asyncHandler(async (req, res) => {
-    //get token from header
-    const token = getTokenFromHeader(req);
-    //verify token
-    const verified = verifyToken(token);
-    console.log(req);
-   
+    //find the user
+    const user = await User.findById(req.userAuthId).populate("orders");
+
+    
     res.json({
-        msg: "Welcome to profile page"
+        status: 'success',
+        message: 'User profile fetched successfully',
+        user,
     });
 });
 
