1d46ac01ad808143baba4a47a9f35e664a2e4c78
ishi12121 <isht1402@gmail.com>
2023-11-13 06:29:20 +0530

    Add order CRUD functionality to backend

--- backend/controllers/orderCtrl.js
+++ backend/controllers/orderCtrl.js
@@ -80,3 +80,110 @@ export const createOrderCtrl = asyncHandler(async (req, res) => {
   
 
 });
+
+//@desc get all orders
+//@route GET /api/v1/orders
+//@access private
+
+export const getAllOrdersCtrl = asyncHandler(async (req, res) => {
+  //find all orders
+  const orders = await Order.find();
+  res.json({
+    success: true,
+    message: "all orders",
+    orders,
+  });
+});
+//@desc get single order
+//@route GET /api/v1/orders
+//@access private/admin
+export const getSingleOrderCtrl = asyncHandler(async (req, res) => {
+  //find order by id from params
+  const id = req.params.id
+  const order = await Order.findById(id);
+  // send response
+  res.json({
+    success: true,
+    message: "order fetched successfully",
+    order,
+  })
+
+});
+
+//@desc update order to delivered
+//@route PUT /api/v1/orders/update/:id
+//@access private/admin
+
+export const updateOrderCtrl = asyncHandler(async (req, res) => {
+  //get the id from params
+  const id = req.params.id;
+  //update
+  const updatedOrder = await Order.findByIdAndUpdate(
+    id,
+    {
+      status: req.body.status,
+    },
+    {
+      new: true,
+    }
+  );
+  res.status(200).json({
+    success: true,
+    message: "Order updated",
+    updatedOrder,
+  });
+});
+
+//@desc get sales sum of orders
+//@route GET /api/v1/orders/sales/sum
+//@access private/admin
+
+export const getOrderStatsCtrl = asyncHandler(async (req, res) => {
+  //get order stats
+  const orders = await Order.aggregate([
+    {
+      $group: {
+        _id: null,
+        minimumSale: {
+          $min: "$totalPrice",
+        },
+        totalSales: {
+          $sum: "$totalPrice",
+        },
+        maxSale: {
+          $max: "$totalPrice",
+        },
+        avgSale: {
+          $avg: "$totalPrice",
+        },
+      },
+    },
+  ]);
+  //get the date
+  const date = new Date();
+  const today = new Date(date.getFullYear(), date.getMonth(), date.getDate());
+  const saleToday = await Order.aggregate([
+    {
+      $match: {
+        createdAt: {
+          $gte: today,
+        },
+      },
+    },
+    {
+      $group: {
+        _id: null,
+        totalSales: {
+          $sum: "$totalPrice",
+        },
+      },
+    },
+  ]);
+  //send response
+  res.status(200).json({
+    success: true,
+    message: "Sum of orders",
+    orders,
+    saleToday,
+  });
+});
--- backend/routes/ordersRouter.js
+++ backend/routes/ordersRouter.js
@@ -1,7 +1,11 @@
 import express from "express";
 import { isLoggedIn } from "../middlewares/isLoggedIn.js";
-import { createOrderCtrl } from "../controllers/orderCtrl.js";
+import { createOrderCtrl, getAllOrdersCtrl, getOrderStatsCtrl, getSingleOrderCtrl, updateOrderCtrl } from "../controllers/orderCtrl.js";
 const orderRouter = express.Router();
 
-orderRouter.post("/", isLoggedIn,  createOrderCtrl);
+orderRouter.post("/", isLoggedIn, createOrderCtrl);
+orderRouter.get("/", isLoggedIn, getAllOrdersCtrl);
+orderRouter.get("/:id", isLoggedIn, getSingleOrderCtrl);
+orderRouter.put("/update/:id", isLoggedIn, updateOrderCtrl);
+orderRouter.get("/sales/sum", isLoggedIn, getOrderStatsCtrl);
 export default orderRouter;
